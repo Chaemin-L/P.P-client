563ddf3cbd3d7bfb0e099baf43d40fed8624ffe7
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2024-03-19 19:01:14 +0900

    fix: 메세지 send mutation 오류 해결

--- src/hooks/queries/useSendMessages.tsx
+++ src/hooks/queries/useSendMessages.tsx
@@ -1,13 +1,10 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { AxiosResponse, AxiosError } from "axios";
+import { useMutation } from "@tanstack/react-query";
 
 import ChatApi from "@/api/chat-api";
 import { ChatSendResponse } from "@/api/type";
 
 export const UseSendMessages = (sendData: ChatSendResponse) => {
-  const queryClient = useQueryClient();
-  const mutation = useMutation({
+  return useMutation({
     mutationFn: () => ChatApi.sendChatMessages(sendData),
-    onSuccess: () => {},
   });
 };
