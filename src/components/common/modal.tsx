1b3d747bdb423ae7ffdcf986b33aabf99d68a78c
Chaemin-L <chaem12@kookmin.ac.kr>
2024-04-02 02:49:33 +0900

    feat: modal 공통 컴포넌트 구현

---
+++ src/components/common/modal.tsx
@@ -0,0 +1,115 @@
+import { ButtonHTMLAttributes, MouseEventHandler, ReactNode } from "react";
+import { createPortal } from "react-dom";
+import { styled } from "styled-components";
+
+import { ReactComponent as CloseSVG } from "@/assets/icons/close.svg";
+
+type CloseButtonType = {
+  onClick: MouseEventHandler<HTMLDivElement>;
+};
+
+type TitleType = {
+  text: string;
+};
+
+type ButtonType = Omit<ButtonHTMLAttributes<HTMLButtonElement>, "type">;
+
+type ModalType = {
+  onClose: MouseEventHandler<HTMLDivElement>;
+  children: ReactNode;
+};
+
+const CloseButton = ({ onClick }: CloseButtonType) => (
+  <CloseButtonWrapper className="modal-close-button" onClick={onClick}>
+    <CloseSVG />
+  </CloseButtonWrapper>
+);
+
+const Title = ({ text }: TitleType) => {
+  return (
+    <TitleWrapper>
+      {text.split("\\n").map((t) => (
+        <span key={t}>{t}</span>
+      ))}
+    </TitleWrapper>
+  );
+};
+
+const Button = ({ children, ...props }: ButtonType) => {
+  return <ButtonWrapper {...props}>{children}</ButtonWrapper>;
+};
+
+export const Modal = ({ onClose, children }: ModalType) => {
+  return (
+    <>
+      {createPortal(
+        <ModalBackground>
+          <Content>
+            <CloseButton onClick={onClose} />
+            {children}
+          </Content>
+        </ModalBackground>,
+        document.body,
+      )}
+    </>
+  );
+};
+
+Modal.Button = Button;
+Modal.Title = Title;
+
+const CloseButtonWrapper = styled.div`
+  width: 64px;
+  height: 64px;
+  position: absolute;
+  background-color: transparent;
+  top: -15px;
+  right: -10px;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  word-wrap: break-word;
+`;
+
+const ButtonWrapper = styled.button`
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: #f17547;
+  border: 0;
+  border-radius: 30px;
+  color: white;
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const ModalBackground = styled.div`
+  position: absolute;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(217, 217, 217, 0.7);
+`;
+
+const Content = styled.div`
+  position: relative;
+  display: flex;
+  max-width: 90%;
+  max-height: 90%;
+  padding: 52px 34px 40px;
+  background-color: #f5f5f5;
+  border-radius: 35px;
+  box-shadow: 0px 4px 30px 0px rgba(0, 0, 0, 0.25);
+  font-size: 25px;
+  flex-direction: column;
+  text-align: center;
+  gap: 30px;
+  & > * {
+    flex: 1;
+  }
+`;
--- src/pages/post/post-detail.tsx
+++ src/pages/post/post-detail.tsx
@@ -4,6 +4,7 @@ import { styled } from "styled-components";
 import { ActivityBox } from "@/components/common/activity-box";
 import { BottomFixed } from "@/components/common/bottom-fixed";
 import { Button } from "@/components/common/button";
+import { Modal } from "@/components/common/modal";
 import { DefaultLayout } from "@/components/layout/default-layout";
 import activityData from "@/data/activity-data.json";
 
@@ -14,14 +15,21 @@ export const PostDetailPage = () => {
 
   // TODO: 자신이 신청한 게시글인지 확인하는 로직
   const [apply, setApply] = useState<boolean>(false);
+  const [reportModal, setReportModal] = useState<boolean>(false);
 
   return (
     <DefaultLayout>
       <ActivityBox {...activityData} />
       <ButtonWrapper>
-        <Button primary isSmall onClick={() => console.log("신고합니다")}>
+        <Button primary isSmall onClick={() => setReportModal(true)}>
           신고
         </Button>
+        {reportModal && (
+          <Modal onClose={() => setReportModal(false)}>
+            <Modal.Title text="신청을\n취소하시겠습니까?" />
+            <Modal.Button>취소하기</Modal.Button>
+          </Modal>
+        )}
       </ButtonWrapper>
       <BottomFixed>
         <BottomFixed.Button
