0341e506244114161724ae1639ac2a338e3267e9
Chaemin-L <chaem12@kookmin.ac.kr>
2024-05-07 22:17:04 +0900

    chore: 서버 측 요청으로 에러페이지 라우팅 제거

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import { GlobalLayout } from "@/components/layout/global-layout";
 import { KakaoAuth } from "@/components/login/kakao-auth";
 import { Chat } from "@/pages/chat/chat";
 import { ChatRoom } from "@/pages/chat/chat-room";
-import { WrongAccessPage } from "@/pages/error/wrong-access";
+// import { WrongAccessPage } from "@/pages/error/wrong-access";
 import { LoginPage } from "@/pages/login";
 import { Mypage } from "@/pages/mypage/mypage";
 import { ApplicantListPage } from "@/pages/post/applicant-list-page";
@@ -135,7 +135,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <GlobalLayout />,
-    errorElement: <WrongAccessPage />,
+    // errorElement: <WrongAccessPage />,
     children: routeChildren.map(({ path, element, auth }) => {
       if (auth) return { path, element: <AuthLayout>{element}</AuthLayout> };
       return { path, element };
